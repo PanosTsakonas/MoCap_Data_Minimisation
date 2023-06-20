03541092fabdb90a3d1d68de333475a257bcc45a
Panos <64256997+PanosTsakonas@users.noreply.github.com>
2023-06-20 13:08:15 +0100

    Add files via upload

--- myobj.m
+++ myobj.m
@@ -1,23 +1,32 @@
-function [obj,th_exp]=myobj(M1,M2,M3,L1,L2,L3,EXT,FL1,FL2,g,r,tim,init,th_G,i,I,theq,grav,in)
+function [obj,th_exp]=myobj(M1,M2,M3,L1,L2,L3,EXT,FL1,FL2,g,r,tim,init,th_G,i,I,theq,grav,in,Boot)
 %This functions minimises the sum of the square differences between the IBK
 %approximation for each degree of freedom and the filtered angular data obtained from the
 %motion capture laboratory.
 if in~=1
 if i==1
+    if Boot==0
     disp("Minimising the parameters for the movement of the proximal segment");
+    else
+    end
 [~,Y]=ode45(@(t,y) IBK_th1(M1,M2,M3,L1,EXT,FL1,FL2,g,r,I(1),theq,grav,t,y),tim,[init(1) init(2)]);
 %[~,Y]=ode45(IBK_th1(M1,M2,M3,L1,EXT,FL1,FL2,g,r,I(1),theq,grav,[],[]),tim,[init(1) init(2)]);
 th_exp=Y(:,1);
 
 elseif i==2
+    if Boot==0
     disp("Minimising the parameters for the movement of the middle segment");
+    else
+    end
     [~,Y]=ode45(@(t,y) IBK_th2(M2,M3,L2,EXT,FL1,FL2,g,r,I(2),theq,grav,t,y),tim,[init(3) init(4)]);
     %[~,Y]=ode45(IBK_th2(M2,M3,L2,EXT,FL1,FL2,g,r,I(2),theq,grav,[],[]),tim,[init(1) init(2)]);
     th_exp=Y(:,1);
     
 
 else
+    if Boot==0
     disp("Minimising the parameters for the movement of the distal segment");
+    else
+    end
     [~,Y]=ode45(@(t,y) IBK_th3(M3,L3,EXT,FL1,g,r,I(3),theq,grav,t,y),tim,[init(5) init(6)]);
     %[~,Y]=ode45(IBK_th3(M3,L3,EXT,FL1,g,r,I(3),theq,grav,[],[]),tim,[init(1) init(2)]);
 th_exp=Y(:,1);
@@ -40,6 +49,9 @@ end
 
 %Least squares
 obj=sum(sum((th_exp-th_G).^2));
+
+%Root mean square error
+%obj=sqrt(mean((th_exp-th_G).^2));
 end
 
 

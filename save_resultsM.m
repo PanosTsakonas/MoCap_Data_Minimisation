468c6549822c570eac781496b5aa8a616ba26b8b
Panos <64256997+PanosTsakonas@users.noreply.github.com>
2023-06-20 13:04:15 +0100

    save_resultsM.m

--- save_resultsM.m
+++ save_resultsM.m
@@ -7,7 +7,7 @@ m={Min1(1) sd_mcp(1) Min1(2) sd_mcp(2) Min1(3) sd_mcp(3) Min1(4) sd_mcp(4) Min1(
 p={Min2(1) sd_pip(1) Min2(2) sd_pip(2) Min2(3) sd_pip(3) Min2(4) sd_pip(4) Min2(5) sd_pip(5) "R^2 :"+rsq(2)+" RMSE: "+rmse(2)+" deg"};
 d={Min3(1) sd_dip(1) Min3(2) sd_dip(2) Min3(3) sd_dip(3) Min3(4) sd_dip(4) "" ""  "R^2 :"+rsq(3)+" RMSE: "+rmse(3)+" deg"};
 M=[{'MN1';mn1} {'MN2';mn2} {'MN3';mn3} {'MCP_cutoff (Hz)';wn_mcp} {'PIP_cutoff (Hz)';wn_pip} {'DIP_cutoff (Hz)';wn_dip}];
-file="C:\Users\panos\OneDrive - University of Warwick\PhD\Hand Trials\Results\Cylindrical Grasp\P"+Par+"\Cylindrical_"+cyl+"_"+trial+"_Par_"+Par+".xlsx";
+file="P"+Par+"\Cylindrical_"+cyl+"_"+trial+"_Par_"+Par+".xlsx";
 
 if in==2
 writecell(param,file,'Sheet',1,'Range','B1');
